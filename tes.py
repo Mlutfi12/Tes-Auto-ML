c5ddc4f08a3ce4572df054fa4b40ecf9f83dd70d
Muhammad Lutfi <luthfimuhammad543@yahoo.com>
2023-09-10 13:26:01 +0700

    tes aja

--- tes.py
+++ tes.py
@@ -1,1 +1,15 @@
-print(1+1)
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+import random
+import math
+import sys
+import os
+import copy
+import csv
+import pandas as pd
+import seaborn as sns
+
+
+#
